c66a6894158c49c0cc8a587692a004e845cb8d1a
Arvydas_Mickus <amickus@icloud.com>
2020-04-08 12:47:39 +0300

    build files added

---
+++ build/precache-manifest.4b0b285e60ad8f7e2031b8f43a9507cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bbb0fd9c2185a3e31ed98b84a5c774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e1490af6c0ea2b41e55",
+    "url": "/static/css/main.3daac228.chunk.css"
+  },
+  {
+    "revision": "7d4d5b83c35bbeb30ea8",
+    "url": "/static/js/2.a14db74d.chunk.js"
+  },
+  {
+    "revision": "b8df9dcff877cce9663782d3a5799833",
+    "url": "/static/js/2.a14db74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1490af6c0ea2b41e55",
+    "url": "/static/js/main.110a0ec7.chunk.js"
+  },
+  {
+    "revision": "dd31b070aa021b33af2e",
+    "url": "/static/js/runtime-main.38f73827.js"
+  }
+]);
